09852260f7d28294ff73c0f18fa1500db0b2343e
Project Crew <user@domain.com>
2026-09-14 00:37:06 -0400

    fix height

--- src/coni.py
+++ src/coni.py
@@ -3,22 +3,31 @@
 # Base on work of Tesla coils "community"
 # Project Crew™ 9/6/2026
 
-from math import pi, pow, sqrt, sin, cos, tan
+from math import pi, pow, sqrt, sin, cos, tan, atan
 
-deg2rad = lambda deg: deg * pi / 180.0  # Convert degrees to radians
+# deg2rad = lambda deg: deg * pi / 180.0  # Convert degrees to radians
 
 # measure twice, calculate n*unce
 # units are the millimeters (mm)
-N  = 7.68                               # turns count
-a  = deg2rad(20)                        # slope angle
-R  = 56                                 # mean radius
-H  = tan(a) * R                         # height
-W  = 64.9                               # from turns start to outer edge
-Ri = 23.55                              # inner radius
-Ro = 88.45                              # outer radius
+# from center (+) of the wire
+N   = 7.68                              # turns count
+H   = 32.193                            # height
+W   = 64.9                              # from turns start to outer edge
+D   = 176.9                             # outer diameter
+Ro  = D / 2                             # outer radius
+Ri  = Ro - W                            # inner radius
+a   = atan(H / Ro)                      # slope angle in radians
+R   = (Ri + Ro) / 2                     # mean radius
+p_v = tan(a) * R / N                    # pitch of winding
+p   = W / N
 
-# Wheeler25/Terman43 in millimeters
+# Wheeler28/Terman43 in millimeters
 L1 = 5 * pow( (N * R), 2 ) / (1143 * R + 1270 * H)  # Solenoid component
 L2 = 5 * pow( (N * R), 2 ) / (1016 * R + 1397 * W)  # Planar     ...
+# print( 'L1 =', L1, ': L2 =', L2)
 print( 'L = {:0.3f}'.format( sqrt( pow(L1 * sin(a), 2) + pow(L2 * cos(a), 2) ) ), '\u00b5H' )
-print( 'N =', sqrt( 1143 * L1 * R + 1270 * H * L1 ) / (sqrt(5) * R) )
+
+# i  = sqrt( 635 * L1 ** 2 * p_v ** 2 + 9 * L1 * R ** 3 )
+# print( 'N =', ( sqrt(635) * i + 635 * L1 * p_v ) / (5 * R ** 2) )
+# L1 = pow(L1 * sin(a), 2)
+# print(sqrt((580644*p_v*L1*R**2*sin(a))/(127*sqrt(L1)*(5*sin(a)**2*R**3)*p_v)+(64516*p_v**2*L1)/(sin(a)**2*R**4))/2+(127*sqrt(L1)*p_v)/(R**2*sin(a)))
